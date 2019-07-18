bc9458cb73ed43659c48a0bf560d369a9356a4c8
Jenesh <jenesh.napit@gmail.com>
2019-07-17 21:38:27 -0400

    rendered to DOM

--- script.js
+++ script.js
@@ -14,13 +14,39 @@ request.onload = function () {
 
     if (request.status >= 200 && request.status < 400) {
         data.forEach(movie => {
-            // Log each movie's title
-            console.log(movie.title);
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0, 310);
+            p.textContent = `${movie.description}...`;
+
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);
         })
     } else {
-        console.log('error');
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Sorry, try again later`;
+        app.appendChild(errorMessage);
     }
 }
 
 // Send request
 request.send();
+
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+
+logo.src = './assets/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
